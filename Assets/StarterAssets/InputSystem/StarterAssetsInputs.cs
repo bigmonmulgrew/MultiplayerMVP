c2ab36d01268575381f543bcc7c176a80fbfcd77
David Mulgrew <bigmonmulgrew@users.noreply.github.com>
2025-09-28 15:57:23 +0100

    autosave

--- Assets/StarterAssets/InputSystem/StarterAssetsInputs.cs
+++ Assets/StarterAssets/InputSystem/StarterAssetsInputs.cs
@@ -1,29 +1,27 @@
-using UnityEngine;
-#if ENABLE_INPUT_SYSTEM
-using UnityEngine.InputSystem;
-#endif
 using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace StarterAssets
 {
-	public class StarterAssetsInputs : NetworkBehaviour
+    public class StarterAssetsInputs : NetworkBehaviour
     {
-		[Header("Character Input Values")]
-		public Vector2 move;
-		public Vector2 look;
-		public bool jump;
-		public bool sprint;
+        [Header("Character Input Values")]
+        public Vector2 move;
+        public Vector2 look;
+        public bool jump;
+        public bool sprint;
 
-		[Header("Movement Settings")]
-		public bool analogMovement;
+        [Header("Movement Settings")]
+        public bool analogMovement;
 
-		[Header("Mouse Cursor Settings")]
-		public bool cursorLocked = true;
-		public bool cursorInputForLook = true;
-
+        [Header("Mouse Cursor Settings")]
+        public bool cursorLocked = true;
+        public bool cursorInputForLook = true;
 
         public void OnMove(InputAction.CallbackContext context)
         {
+            Debug.Log("Is Owner:" + IsOwner);
             if (!IsOwner) return;
             MoveInput(context.ReadValue<Vector2>());
         }
@@ -47,37 +45,15 @@ namespace StarterAssets
             SprintInput(context.ReadValueAsButton());
         }
 
+        public void MoveInput(Vector2 newMoveDirection) => move = newMoveDirection;
+        public void LookInput(Vector2 newLookDirection) => look = newLookDirection;
+        public void JumpInput(bool newJumpState) => jump = newJumpState;
+        public void SprintInput(bool newSprintState) => sprint = newSprintState;
 
+        private void OnApplicationFocus(bool hasFocus) =>
+            SetCursorState(cursorLocked);
 
-        public void MoveInput(Vector2 newMoveDirection)
-		{
-			move = newMoveDirection;
-		} 
-
-		public void LookInput(Vector2 newLookDirection)
-		{
-			look = newLookDirection;
-		}
-
-		public void JumpInput(bool newJumpState)
-		{
-			jump = newJumpState;
-		}
-
-		public void SprintInput(bool newSprintState)
-		{
-			sprint = newSprintState;
-		}
-
-		private void OnApplicationFocus(bool hasFocus)
-		{
-			SetCursorState(cursorLocked);
-		}
-
-		private void SetCursorState(bool newState)
-		{
-			Cursor.lockState = newState ? CursorLockMode.Locked : CursorLockMode.None;
-		}
-	}
-	
+        private void SetCursorState(bool newState) =>
+            Cursor.lockState = newState ? CursorLockMode.Locked : CursorLockMode.None;
+    }
 }
